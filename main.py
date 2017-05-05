f1644729325c81b2311136857b098a42f8620b16
Edgar <hi.ed@hotmail.com>
2017-05-05 15:02:15 -0500

    antes de que la riege

--- main.py
+++ main.py
@@ -25,22 +25,47 @@ for pieza_info in renglones:
     piezas.append(nuevaPieza)
     i += 1
 
-piezas.sort(key=attrgetter("alto"), reverse=True)
+piezas.sort(key=attrgetter("alto"), reverse=False)
 
 for pieza in piezas:
     telas.append(Tela(int(pieza.getalto()), ancho_del_telon))
 
-piezas.sort(key=attrgetter("area"), reverse=True)
+piezas.sort(key=attrgetter("area"), reverse=False)
+
+print("Piezas\n")
 
 
+'''
 for pieza in piezas:
     for tela in telas:
         if pieza.getancho() <= tela.getlibre() and pieza.getalto() <= tela.getaltura() and pieza.getasignada() is False:
             pieza.setasignada()
             tela.coser(pieza.getid())
             tela.setusado(pieza.getancho())
+'''
 
+contador = 0
+i_pieza = 0
+i_tela = 0
+while i_pieza < len(piezas):
+    if piezas[i_pieza].getancho() <= telas[i_tela].getlibre() and piezas[i_pieza].getalto() <= telas[i_tela].getaltura() and piezas[i_pieza].getasignada() is False:
+        piezas[i_pieza].setasignada()
+        telas[i_tela].coser(piezas[i_pieza].getid())
+        telas[i_tela].setusado(piezas[i_pieza].getancho())
+        i_pieza += 1
+    else:
+        i_tela += 1
+    if i_tela == len(piezas):
+        print("Sobrepaso!")
 
 print("\nTelas\n")
+
+altura_total_si = 0
+
 for tela in telas:
-    print (tela.tostring())
+    print(tela.tostring())
+    if tela.getcosidos() != []:
+        altura_total_si += tela.getaltura()
+
+print("\n")
+print("La altura del telón fue de " + str(altura_total_si) + " Unidades  ")
