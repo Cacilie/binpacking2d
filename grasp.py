f669569be5f527340b1d972d0718c82adef603d9
Edgar <hi.ed@hotmail.com>
2017-05-08 13:55:36 -0500

    i can get a different solution

--- grasp.py
+++ grasp.py
@@ -34,4 +34,31 @@ def printPiezas():
 
 def calcularPSoluciones():
     global piezas_l, l_piezas, p_soluciones
-    
+    ids_seleccionados = [] # IDs que ya han sido seleccionados para formar parte de una solución
+    lista_areas = [p.getarea() for p in l_piezas]
+    menor = max(lista_areas)
+    mayor = max(lista_areas)
+    menor_id = 0
+    cce = 0
+    cmin = 0
+    alfa = 0.5
+    while len(ids_seleccionados) < len(l_piezas):
+        posibles_agregar = []
+        menor  = max(lista_areas)
+        for p in l_piezas:
+            if p.getarea() <= menor and p.getid() not in ids_seleccionados:
+                menor_id = p.getid()
+                cmin = p.getarea()
+        # ids_seleccionados.append(menor_id)
+        cce = cmin + (alfa * ( mayor - cmin))
+        for p in l_piezas:
+            if p.getarea() >= cmin and p.getarea() <= cce and p.getid() not in ids_seleccionados:
+                posibles_agregar.append(p.getid())
+        if posibles_agregar == []:
+            for p in l_piezas:
+                if p.getid() not in ids_seleccionados:
+                    posibles_agregar.append(p.getid())
+        eleccion = random.randrange(len(posibles_agregar))
+        ids_seleccionados.append(posibles_agregar[eleccion])
+
+    print(str(ids_seleccionados))
